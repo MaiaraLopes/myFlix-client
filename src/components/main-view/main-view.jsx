9d4817a45116a1abdf1e2e8114990ba1a2a893ad
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2022-02-28 17:39:04 +0000

    Applied bootstrap to start the styling

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
@@ -56,17 +58,24 @@ class MainView extends React.Component {
 
         if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} toggleRegistrationView={this.toggleRegistrationView}/>;
 
-        if (selectedMovie) return <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => {
-            this.setSelectedMovie(newSelectedMovie);
-        }} />;
-        
         if (movies.length === 0) return <div className='main-view' />;
-
+        
         return (
-            <div className='main-view'>
-                {movies.map(movie => <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />)}
-            </div>
-        );
+            <Row className='main-view justify-content-md-center'>
+                {selectedMovie
+                    ? (
+                            <Col md={8}>
+                                <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+                            </Col>
+                    )
+                    : movies.map(movie => (
+                                <Col md={3}>
+                                <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+                            </Col>
+                            ))}
+                    
+            </Row>
+        );        
     }
 }
 
