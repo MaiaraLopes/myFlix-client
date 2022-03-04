21b43c79ab399330ad54bca27fd9a5718da80143
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2022-03-04 19:11:44 +0000

    Updated navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,39 +2,14 @@ import React from "react";
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
 
 export function Navbar({ user }) {
-  const onLoggedOut = () => {
-    return localStorage.clear();
-  };
-
-  const isAuth = () => {
-    if (typeof window == "undefined") {
-      return false;
-    }
-    if (localStorage.getItem("token")) {
-      return localStorage.getItem("token");
-    } else {
-      return false;
-    }
-  };
-
   return (
-    <Navbar className="main-nav" sticky="top" bg="light" variant="light">
+    <Navbar bg="light" variant="light">
       <Container>
-        <Navbar.Brand href="#home">myFlix</Navbar.Brand>
+        <Navbar.Brand href="#">myFlix</Navbar.Brand>
         <Nav className="me-auto">
-          {isAuth() && <Nav.Link href={`/users/${user}`}>{user}</Nav.Link>}
-          {isAuth() && (
-            <Button
-              variant="link"
-              onClick={() => {
-                onLoggedOut();
-              }}
-            >
-              Logout
-            </Button>
-          )}
-          {!isAuth() && <Nav.Link href="/">Sing in</Nav.Link>}
-          {!isAuth() && <Nav.Link href="/register">Sing up</Nav.Link>}
+          <Nav.Link href="#">Sign in</Nav.Link>
+          <Nav.Link href="#">Register</Nav.Link>
+          <Button>Sign out</Button>
         </Nav>
       </Container>
     </Navbar>
