4bdbc823f9f0741a450fb1f3550de70f673cd2e2
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2022-03-08 18:41:11 +0000

    Fixed {}

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -77,111 +77,111 @@ export function ProfileView({
           console.log("Error updating user");
         });
     }
-
-    const handleDeleteBtn = () => {
-      axios
-        .delete(
-          `https://myflix-ml.herokuapp.com/users/${oldUserData.Username}`,
-
-          {
-            headers: { Authorization: `Bearer ${token}` },
-          }
-        )
-        .then(() => {
-          onLoggedOut();
-          window.open("/", "_self");
-        })
-        .catch((e) => {
-          console.log("Error deleting user");
-        });
-    };
-
-    return (
-      <CardGroup>
-        <Card>
-          <Card.Body>
-            <Form>
-              <Form.Group>
-                <Form.Label className="mt-2">Username: </Form.Label>
-                <Form.Control
-                  type="text"
-                  value={newUsername}
-                  onChange={(e) => {
-                    setNewUsername(e.target.value);
-                  }}
-                />
-                {usernameErr && <p>{usernameErr}</p>}
-              </Form.Group>
-
-              <Form.Group>
-                <Form.Label className="mt-2">Password: </Form.Label>
-                <Form.Control
-                  type="text"
-                  value={newPassword}
-                  onChange={(e) => {
-                    setNewPassword(e.target.value);
-                  }}
-                />
-                {passwordErr && <p>{passwordErr}</p>}
-              </Form.Group>
-
-              <Form.Group>
-                <Form.Label className="mt-2">Email: </Form.Label>
-                <Form.Control
-                  type="text"
-                  value={newEmail}
-                  onChange={(e) => {
-                    setNewEmail(e.target.value);
-                  }}
-                />
-                {emailErr && <p>{emailErr}</p>}
-              </Form.Group>
-
-              <Form.Group>
-                <Form.Label className="mt-2">Birthdate: </Form.Label>
-                <Form.Control
-                  type="text"
-                  value={newBirthdate}
-                  onChange={(e) => {
-                    setNewBirthdate(e.target.value);
-                  }}
-                />
-                {birthdateErr && <p>{birthdateErr}</p>}
-              </Form.Group>
-
-              <Button
-                className="mt-3 custom-btn-2"
-                variant="custom"
-                onClick={() => {
-                  onBackClick();
-                }}
-              >
-                Back
-              </Button>
-
-              <Button
-                className="mt-3 ml-2 custom-btn"
-                variant="custom"
-                onClick={() => {
-                  handleUpdateBtn();
-                }}
-              >
-                Update
-              </Button>
-
-              <Button
-                className="mt-3 ml-2 custom-btn-2"
-                variant="custom"
-                onClick={() => {
-                  handleDeleteBtn();
-                }}
-              >
-                Delete
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-      </CardGroup>
-    );
   };
+
+  const handleDeleteBtn = () => {
+    axios
+      .delete(
+        `https://myflix-ml.herokuapp.com/users/${oldUserData.Username}`,
+
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+      .then(() => {
+        onLoggedOut();
+        window.open("/", "_self");
+      })
+      .catch((e) => {
+        console.log("Error deleting user");
+      });
+  };
+
+  return (
+    <CardGroup>
+      <Card>
+        <Card.Body>
+          <Form>
+            <Form.Group>
+              <Form.Label className="mt-2">Username: </Form.Label>
+              <Form.Control
+                type="text"
+                value={newUsername}
+                onChange={(e) => {
+                  setNewUsername(e.target.value);
+                }}
+              />
+              {usernameErr && <p>{usernameErr}</p>}
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label className="mt-2">Password: </Form.Label>
+              <Form.Control
+                type="text"
+                value={newPassword}
+                onChange={(e) => {
+                  setNewPassword(e.target.value);
+                }}
+              />
+              {passwordErr && <p>{passwordErr}</p>}
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label className="mt-2">Email: </Form.Label>
+              <Form.Control
+                type="text"
+                value={newEmail}
+                onChange={(e) => {
+                  setNewEmail(e.target.value);
+                }}
+              />
+              {emailErr && <p>{emailErr}</p>}
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label className="mt-2">Birthdate: </Form.Label>
+              <Form.Control
+                type="text"
+                value={newBirthdate}
+                onChange={(e) => {
+                  setNewBirthdate(e.target.value);
+                }}
+              />
+              {birthdateErr && <p>{birthdateErr}</p>}
+            </Form.Group>
+
+            <Button
+              className="mt-3 custom-btn-2"
+              variant="custom"
+              onClick={() => {
+                onBackClick();
+              }}
+            >
+              Back
+            </Button>
+
+            <Button
+              className="mt-3 ml-2 custom-btn"
+              variant="custom"
+              onClick={() => {
+                handleUpdateBtn();
+              }}
+            >
+              Update
+            </Button>
+
+            <Button
+              className="mt-3 ml-2 custom-btn-2"
+              variant="custom"
+              onClick={() => {
+                handleDeleteBtn();
+              }}
+            >
+              Delete
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </CardGroup>
+  );
 }
